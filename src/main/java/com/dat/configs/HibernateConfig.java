4296155ec72c91e76896908e73e500da452daeca
ngodinhdat258@gmail.com <ngodinhdat258@gmail.com>
2024-04-10 03:01:07 +0700

    first

---
+++ src/main/java/com/dat/configs/HibernateConfig.java
@@ -0,0 +1,14 @@
+package com.dat.configs;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class HibernateConfig {
+    
+}
